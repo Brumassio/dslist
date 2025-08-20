1041ae3fc8c44d353e185fdf4460e52cc06ef573
Brumassio <dibrunspeed@gmail.com>
2025-08-19 23:32:01 -0300

    Game seed

--- src/main/java/com/springproject/dslist/Game.java
+++ src/main/java/com/springproject/dslist/Game.java
@@ -14,18 +14,26 @@ public class Game {
     @Column(name = "game_year")
     private Integer year;
     private String genre;
+    private String platforms;
+    private String score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
     }
 
-    public Game(long id, String title, Integer year, String genre, String imgUrl, String shortDescription, String longDescription) {
+    public Game(long id, String title, Integer year, String genre, String platforms, String score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -85,6 +93,22 @@ public class Game {
 
     public void setLongDescription(String longDescription) {
         this.longDescription = longDescription;
+    }
+
+    public String getPlatforms() {
+        return platforms;
+    }
+
+    public void setPlatforms(String platform) {
+        this.platforms = platform;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
     }
 
     @Override
